579b04ec48b82d8d11d5d5808c613244f6b238f5
Csilla Pap <csill.pap@gmail.com>
2017-10-05 13:34:59 +0100

    Add the account app with the registration form

---
+++ accounts/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import messages, auth
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.template.context_processors import csrf
+from accounts.forms import UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = auth.authenticate(username=request.POST.get('username'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                messages.success(request, "You have successfully registered and are now logged into your account")
+                return redirect(reverse('profile'))
+
+            else:
+                messages.error(request, "Sorry, we are unable to log you in at this time!")
+
+    else:
+        form = UserRegistrationForm()
+
+    args = {'form': form}
+    args.update(csrf(request))
+
+    return render(request, 'register.html', args)
