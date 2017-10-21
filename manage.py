02b12ed8bf82db99dc2cb563e6ae627515b31e56
Csilla Pap <csill.pap@gmail.com>
2017-10-21 20:58:07 +0100

    Refactor app name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "know_your_owner.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dog_talk.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
