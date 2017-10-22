f632123ad3da0fa4cdb4e97bfc88c13fc447c0b5
Csilla Pap <csill.pap@gmail.com>
2017-10-22 17:24:04 +0100

    Change the name of the app

--- lulus_dog_forum/wsgi.py
+++ lulus_dog_forum/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dog_talk.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lulus_dog_forum.settings")
 
 application = get_wsgi_application()
