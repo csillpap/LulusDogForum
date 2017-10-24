1211418ba3589092957a1d338cd47f802c34fd31
Csilla Pap <csill.pap@gmail.com>
2017-10-24 16:23:47 +0100

    Update wsgi.py

--- lulus_dog_forum/wsgi.py
+++ lulus_dog_forum/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lulus_dog_forum.settings1")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.staging")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
